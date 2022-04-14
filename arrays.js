1bfd11ecfccd635b4cb5390049ba9d4e392038cf
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-04-14 11:34:17 -0700

    added arrays story demo 2

--- arrays.js
+++ arrays.js
@@ -2,3 +2,38 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+let backpack = [];
+let belt;
+
+backpack.push('mac n cheese', `hatchet`, `water`);
+backpack.unshift(`some beef jerky`);
+backpack.splice(2, 0, `sleeping back`);
+// belt.push(backpack.splice(3, 1));
+belt = backpack.splice(3, 1);
+
+let inUse = backpack.pop();
+
+// console.log(backpack);
+// console.log(belt);
+// console.log(inUse);
+
+backpack.push(inUse);
+
+// console.log(backpack);
+// console.log(backpack.length);
+
+
+let wifePack = backpack.splice(0, 3);
+// console.log(wifePack);
+
+wifePack.push(`jacket`, `knife`);
+// console.log(wifePack);
+
+for (let i = 0; i < backpack.length; i++) {
+    console.log(backpack[i]);
+}
+
+for (let i = 0; i < wifePack.length; i++) {
+    console.log(wifePack[i]);
+}
